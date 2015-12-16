7441e91f669221d38b4f50458ab31c2ab761d1e3
Lars Brinkhoff <lars@nocrew.org>
2015-12-16 10:49:19 +0100

    WIP: win32 x86.  Don't build x86 target.

--- targets/x86/forth.mk
+++ targets/x86/forth.mk
@@ -3,7 +3,7 @@ META-OUTPUT = $(TFORTH)
 $(TFORTH): b-forth $(DEPS) $(PARAMS) $(META)
 	echo include $(META) | ./forth | tail -n+3 > $@
 	chmod a+x $@
-	cp $@ forth
+	#cp $@ forth
 
 params.fth: $(TDIR)/params.fth
 	cp $^ $@
